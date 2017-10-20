0d9a5ad8b911996f98ce0bdbffd1f1a1ada4b075
Philip10023 <philipormorgan@gmail.com>
2017-10-20 04:35:46 -0400

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,7 +14,17 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New User Connected')
 
-
+  //socket.emit from admin to say welcome to my site
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: `welcome to the chat app :)`
+  })
+  //socket.broastcast.emit from admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
     io.emit('newMessage', {
@@ -22,6 +32,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   frome: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on("disconnect", () => {
