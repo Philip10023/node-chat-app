2c7ca59959677548b67d18baccebc5ffcca17893
Philip10023 <philipormorgan@gmail.com>
2017-10-24 17:22:22 -0400

    scroll to bottom if user is near bottom and switched to mustcache.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const path = require('path')
-const http = require('http')
-const express = require("express")
-const socketIO = require('socket.io')
+const path = require('path');
+const http = require('http');
+const express = require("express");
+const socketIO = require('socket.io');
 
-const {generateMessage, generateLocationMessage} = require('./utils/message')
-const publicPath = path.join(__dirname, '../public')
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express()
-var server = http.createServer(app)
-var io = socketIO(server)
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath))
 
@@ -22,19 +22,19 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message)
-    io.emit('newMessage', generateMessage(message.from, message.text))
-    callback()
-  })
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
   socket.on('createLocationMessage', (coords) => {
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
-  })
+  });
 
   socket.on("disconnect", () => {
     console.log('disconnected from client')
-  })
-})
+  });
+});
 
 server.listen(port, () => {
   console.log(`started on port ${port}`)
-})
+});
