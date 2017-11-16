505a6619a99c393feea68384e053ee1d79a6394c
Philip10023 <philipormorgan@gmail.com>
2017-11-16 16:36:40 -0500

    cleaned up server code

--- server/server.js
+++ server/server.js
@@ -27,36 +27,33 @@ app.use( express.static(public_path) );
 
 //REMOVE ALL USER CONNECTIONS
 
-Room.cleanAllUserList().then( () => {
+Room.cleanAllUserList().then(() => {
   console.log('Rooms were cleaned');
-}).catch( (e) =>{
+}).catch((e) => {
   console.log(e);
 });
 
 io.on('connection', (socket) => {
 
-
   socket.on('join', (params, callback) => {
-
     let user;
 
     //Authenticate user
-    User.findByToken(params.user_token).then( (userDoc) => {
-      if(!userDoc){
+    User.findByToken(params.user_token).then((userDoc) => {
+      if (!userDoc) {
         throw new Error('Invalid user');
       }
       user = userDoc;
 
       //Veirfy room id
       return Room.findById(params.room_id);
-    }).then( (roomDoc) => {
-
+    }).then((roomDoc) => {
       let userList = roomDoc.getUsers();
       //Check if user is not duplicated
       let duplicated = userList.filter( u => u.name == user.name);
-      console.log(duplicated);
 
-      if( duplicated.length > 0){
+      if (duplicated.length > 0) {
+
         throw new Error('Sorry. There is an user with this name, try another room :(');
       }
 
@@ -67,7 +64,7 @@ io.on('connection', (socket) => {
         name: user.name
       });
 
-    }).then( (roomDoc) => {
+    }).then((roomDoc) => {
       //Happy path
       io.to(params.room_id).emit('updateUserList', roomDoc.getUsers());
       // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
@@ -82,22 +79,20 @@ io.on('connection', (socket) => {
       };
 
       callback();
-
-    }).catch( (e) => callback(e.message));
-
+    }).catch((e) => callback(e.message));
   });
 
   socket.on('createMessage', (newMessage, callback) => {
     //Get room
     let tmp_room;
-    Room.findById(newMessage.room_id).then( (roomDoc) => {
+    Room.findById(newMessage.room_id).then((roomDoc) => {
       tmp_room = roomDoc;
-      if(tmp_room && isRealString(newMessage.text)){
+      if (tmp_room && isRealString(newMessage.text)) {
         return roomDoc.addMessage(generateMessage(newMessage.user_name, newMessage.text));
-      }else {
+      } else {
         return Promise.reject();
       }
-    }).then( (messageDoc) => {
+    }).then((messageDoc) => {
       io.to(tmp_room._id).emit('newMessage', generateMessage(newMessage.user_name, newMessage.text));
       callback();
     });
@@ -105,14 +100,14 @@ io.on('connection', (socket) => {
 
   socket.on('createLocationMessage', (newMessage) => {
     let tmp_room;
-    Room.findById(newMessage.room_id).then( (roomDoc) => {
+    Room.findById(newMessage.room_id).then((roomDoc) => {
       tmp_room = roomDoc;
-      if(tmp_room && newMessage.latitude && newMessage.longitude){
+      if (tmp_room && newMessage.latitude && newMessage.longitude) {
         return roomDoc.addMessage(generateLocationMessage(newMessage.user_name,newMessage.latitude, newMessage.longitude));
-      }else {
+      } else {
         return Promise.reject();
       }
-    }).then( (messageDoc) => {
+    }).then((messageDoc) => {
       io.to(tmp_room._id).emit('newMessage', generateLocationMessage(newMessage.user_name,newMessage.latitude, newMessage.longitude));
     });
   });
@@ -125,42 +120,41 @@ io.on('connection', (socket) => {
       password: params.password
     });
 
-    user.save().then( () => {
+    user.save().then(() => {
       return user.generateAuthToken();
-    }).then( (token) => {
+    }).then((token) => {
       callback(null, user, token);
-    }).catch( (e) => {
+    }).catch((e) => {
       callback(e);
-    } );
+    });
   });
 
   socket.on('getRoomList', (callback) => {
 
-    Room.getRoomList().then( (roomList) => {
+    Room.getRoomList().then((roomList) => {
       callback(roomList);
-    }).catch( (e) => {
+    }).catch((e) => {
       callback();
     });
-
   });
 
-  socket.on('getRoom', (params, callback) =>{
+  socket.on('getRoom', (params, callback) => {
 
-    Room.findOne({name: params.name}).then( (roomDoc) => {
+    Room.findOne({name: params.name}).then((roomDoc) => {
       callback(roomDoc);
-    }).catch( (e) => {
+    }).catch((e) => {
       callback();
     });
   });
 
   socket.on('signIn', (userClient, callback) => {
     let temp_user;
-    User.findByCredentials(userClient.email, userClient.password).then( (user) => {
+    User.findByCredentials(userClient.email, userClient.password).then((user) => {
       temp_user = user;
       return user.generateAuthToken();
-    }).then( (token) =>{
+    }).then((token) =>{
       callback(token, temp_user);
-    }).catch( (e) => {
+    }).catch((e) => {
       callback();
     });
   });
@@ -170,20 +164,19 @@ io.on('connection', (socket) => {
     //Returns true if token is removed
 
     User.findByToken(userClient.token).then( (user) =>{
-      if(!user){
+      if (!user) {
         return Promise.reject();
       }
       return user.removeToken(userClient.token);
 
-    }).then( () =>{
+    }).then(() =>{
       callback(true);
-    }).catch( (e) => {
+    }).catch((e) => {
       callback(false);
     });
-
   });
 
-  socket.on('newRoom', (roomClient, callback) =>{
+  socket.on('newRoom', (roomClient, callback) => {
 
     let tmp_newRoom;
 
@@ -191,43 +184,39 @@ io.on('connection', (socket) => {
       name: roomClient.name
     });
 
-    room.save().then( (newRoom) =>{
+    room.save().then((newRoom) => {
       tmp_newRoom = newRoom;
       return Room.getRoomList();
       //updateRoomList client
-
-    }).then( (roomList) => {
+    }).then((roomList) => {
       socket.broadcast.emit('updateRoomList', roomList);
       callback(tmp_newRoom);
-    }).catch( () => {
+    }).catch(() => {
       callback();
     });
-
   });
 
   socket.on('disconnect', () => {
 
-    if( socket._customdata ){
+    if (socket._customdata) {
       let params = socket._customdata;
       let tmp_room;
-      Room.findById(params.room_id).then( (roomDoc) => {
+      Room.findById(params.room_id).then((roomDoc) => {
         tmp_room = roomDoc;
         return tmp_room.removeUser(params.user_id);
-      }).then( (userDoc) => {
-        tmp_room.users = tmp_room.users.filter( user => user._id != params.user_id);
+      }).then((userDoc) => {
+        tmp_room.users = tmp_room.users.filter(user => user._id != params.user_id);
         io.to(params.room_id).emit('updateUserList', tmp_room.users);
         io.to(params.room_id).emit('newMessage', generateMessage('Admin', `${params.user_name} has left.`));
 
         console.log(`${params.user_name} has left room \'${tmp_room.name}\'`);
-      }).catch( (e) => {
+      }).catch((e) => {
         console.log('error:' +e);
       });
     }
-
   });
-
 });
 
-server.listen(port, ()=> {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
